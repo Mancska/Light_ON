e887f9219a4d45f57476f6d856dd232da5b0483d
Mancska <franyokrisz@gmail.com>
2023-05-12 13:48:03 +0200

    meh

--- JatekTer.js
+++ JatekTer.js
@@ -4,7 +4,7 @@ class Jatekter {
   #meret;
   #db;
   #lepes;
-   #szuloElem = $("article");
+   
      
   constructor() {
     this.#allapotLista=[];
@@ -12,7 +12,10 @@ class Jatekter {
     this.#meret=3;
     this.#db = 0;
     const szuloElem = $("article");
-    const asideElem = $("aside");
+    for (let index = 0; index < 9; index++) {
+      const elem = new Elem(szuloElem, index);
+    }
+    
   }
   setAllapotLista() {
     let hossz = this.#meret*3;
@@ -36,7 +39,7 @@ class Jatekter {
   }
   init(){
     for (let index = 0; index < this.#allapotLista.length; index++) {
-      this.szuloElem.append(`<div class="lampa"><p></p></div>`)
+
       
     }
     
